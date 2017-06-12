4da55352285ab8b53824dca674754be083caef04
Kaushal Patel <kaushalpatel922@gmail.com>
2017-06-12 15:23:08 -0400

    routing routes

--- app/app.js
+++ app/app.js
@@ -5,8 +5,46 @@ var dataFile = require('./data/data.json');
 app.set('port', process.env.PORT || 3000);
 
 app.get('/', function(request, response) {
-  response.send('<h1>Virtual Conferences</h1>');
+  response.send(`
+    <h1>Welcome to JS Conferences</h1>
+    <p>A place for JSConf US users who are passionate to talk about their experiences</p>`
+  );
 });
+
+
+app.get('/speakers', function(request, response) {
+  var info = '';
+  dataFile.speakers.forEach(function(item) {
+    info += `
+    <li>
+      ${item.name}
+      <p>${item.summary}</p>
+    </li>
+    `;
+  });
+  response.send(`
+    <h1>JS Conferences</h1>
+    ${info}
+  `);
+});
+
+
+app.get('/speakers/', function(request, response) {
+  var info = '';
+  dataFile.speakers.forEach(function(item) {
+    info += `
+    <li>
+      ${item.name}
+      <p>${item.summary}</p>
+    </li>
+    `;
+  });
+  response.send(`
+    <h1>JS Conferences</h1>
+    ${info}
+  `);
+});
+
 
 var server = app.listen(app.get('port'), function() {
   console.log('listening to port '+ app.get('port'));
