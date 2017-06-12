1b1a13cc524e6cfb29243d3934cd4cc7e36203d2
Kaushal Patel <kaushalpatel922@gmail.com>
2017-06-12 16:30:46 -0400

    fixed css loading

--- app/routes/speakers.js
+++ app/routes/speakers.js
@@ -16,7 +16,7 @@ router.get('/speakers', function(request, response) {
     `;
   });
   response.send(`
-    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <link rel="stylesheet" type="text/css" href="/css/style.css">
     <h1>JS Conferences</h1>
     ${info}
   `);
@@ -27,8 +27,10 @@ router.get('/speakers/:speakerid', function(request, response) {
   var dataFile = request.app.get('appData');
   var speaker = dataFile.speakers[request.params.speakerid];
   response.send(`
+    <link rel="stylesheet" type="text/css" href="/css/style.css">
     <h1>${speaker.title}</h1>
     <h2>with ${speaker.name}</h2>
+    <img src="/images/speakers/${speaker.shortname}.png" alt="speaker">
     <p>${speaker.summary}</p>
   `);
 });
