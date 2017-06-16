694aabce9f43bb0ed268ba6b95b9ab21c40fdb40
Kaushal Patel <kaushalpatel922@gmail.com>
2017-06-16 14:01:27 -0400

    Post feedback working

--- app/routes/api.js
+++ app/routes/api.js
@@ -1,10 +1,26 @@
 var express = require('express');
 var router = express.Router();
+var bodyParser = require('body-parser');
+var fs = require('fs');
 var feedbackData = require('../data/feedback.json');
 
 router.get('/api', function(request, response) {
   response.json(feedbackData);
 });
 
+//To understand request properly
+router.use(bodyParser.json());
+//Read urlencoded data when a form sends information
+router.use(bodyParser.urlencoded({ extended: false }));
+
+router.post('/api', function(request, response) {
+  feedbackData.unshift(request.body);
+  fs.writeFile('app/data/feedback.json', JSON.stringify(feedbackData, 'utf8',
+    function(err) {
+      console.log(err);
+    }
+  )); 
+  response.json(feedbackData);
+});
 
 module.exports = router;
